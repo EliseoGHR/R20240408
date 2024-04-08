511a84e29841935b1b76cb66742719e80ebdd7b0
Eliseo Isaí Serrano Guzmán <sg24002@outlook.com>
2024-04-08 08:37:28 -0600

    Se temino de programar la capa de logica de negocios con sus modelos

---
+++ R20240408.LogicaDeNegocio/PersonaRBL.cs
@@ -0,0 +1,39 @@
+﻿using R20240408.AccesoADatos;
+using R20240408.EntidadesDeNegocio;
+
+namespace R20240408.LogicaDeNegocio
+{
+    public class PersonaRBL
+    {
+        readonly PersonaRDAL _personaRDAL;
+
+        public PersonaRBL(PersonaRDAL personaRDAL)
+        {
+            _personaRDAL = personaRDAL;
+        }
+        public async Task<int> Crear(PersonaR personaR)
+        {
+            return await _personaRDAL.Crear(personaR);
+        }
+        public async Task<int> Modificar(PersonaR personaR)
+        {
+            return await _personaRDAL.Modificar(personaR);
+        }
+        public async Task<int> Eliminar(PersonaR personaR)
+        {
+            return await _personaRDAL.Eliminar(personaR);
+        }
+        public async Task<PersonaR> ObtenerPorId(PersonaR personaR)
+        {
+            return await _personaRDAL.ObtenerPorId(personaR);
+        }
+        public async Task<List<PersonaR>> ObtenerTodos()
+        {
+            return await _personaRDAL.ObtenerTodos();
+        }
+        public async Task<List<PersonaR>> Buscar(PersonaR personaR)
+        {
+            return await _personaRDAL.Buscar(personaR);
+        }
+    }
+}
