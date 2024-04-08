079c50e5fad41ddee8b794ca9dede95fb78a3671
Eliseo Isaí Serrano Guzmán <sg24002@outlook.com>
2024-04-08 08:10:19 -0600

    Se termino de programar la capa de entidades de negocio con  su clase personaR

---
+++ R20240408.EntidadesDeNegocio/PersonaR.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace R20240408.EntidadesDeNegocio
+{
+    public class PersonaR
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "El nombre debe tener entre 2 y 50 caracteres")]
+        [Display(Name = "Nombre")]
+        public string NombreR { get; set; }
+
+        [Required(ErrorMessage = "El Apellido es requerido")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "El Apellido debe tener entre 2 y 50 caracteres")]
+        [Display(Name = "Apellido")]
+        public string ApellidoR { get; set; }     
+
+        [Required(ErrorMessage = "Fecha de Nacimiento es requerido")]
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime FechaNacimientoR { get; set; }
+
+        [Required(ErrorMessage = "Fecha de Nacimiento es requerido")]
+        [Display(Name = "Sueldo")]
+        public decimal SueldoR { get; set; }
+
+        [Required(ErrorMessage = "El estatus  es requerido.")]
+        [Display(Name = "Estatus")]
+        public byte EstatusR { get; set; }
+
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "EL Comentario debe tener entre 2 y 200 caracteres")]
+        [Display(Name = "Comentario")]
+        public string? ComentarioR { get; set; }
+
+    }
+}
---
+++ R20240408.UI.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace R20240408.UI.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
